c9b0f398e58dcd38c1763b21880b3ce7782ad898
j0srisk <j0srisk@gmail.com>
2023-03-24 01:09:03 -0500

    Added eslint

--- src/index.js
+++ src/index.js
@@ -1,110 +1,113 @@
-import 'normalize.css';
-import './styles.css';
+import "normalize.css";
+import "./styles.css";
 
-import Project from './modules/project';
-import Task from './modules/task';
-import UI from './modules/ui';
-import Storage from './modules/storage';
+import Project from "./modules/project";
+import Task from "./modules/task";
+import UI from "./modules/ui";
+import Storage from "./modules/storage";
 
 let projectsList = [];
 let activeProject = null;
 
-function createDefaultProject() {
-    addProject('Inbox');
-    addTask('Create a new project');
-    addTask('Create a new task');
-    addTask('Mark a task as completed');
-}
-
 function updateActiveProject(project) {
-    if (project != null){
-        activeProject = project;
-        UI.renderTasks(activeProject);
-    }
-    
-    return activeProject;
-}
+  if (project != null) {
+    activeProject = project;
+    UI.renderTasks(activeProject);
+  }
 
-function setupEventListeners() {
-    // Project Input Event Listeners
-    const projectSubmitButton = document.querySelector('#saveProjectBtn');
-    projectSubmitButton.addEventListener('click', () => {
-        const projectInputField = document.querySelector('#newProjectInput');
-        const projectTitle  = projectInputField.value.trim();
-        if(projectTitle){
-            addProject(projectTitle);
-        }
-    });
-
-    // Task Input Event Listeners
-    const taskInputField = document.querySelector('#newTaskInput');
-    taskInputField.addEventListener('keydown', (event) => {
-        if(event.key === 'Enter'){
-            event.preventDefault();
-            if (taskInputField.value){
-                addTask(taskInputField.value);
-            }
-        }
-    });
+  return activeProject;
 }
 
 function addProject(title) {
-    const newProject = new Project(title);
-    projectsList.push(newProject);
-    updateActiveProject(newProject);
-    UI.renderProjects(projectsList);
-    UI.renderTasks(activeProject);
-    Storage.save(projectsList);
+  const newProject = new Project(title);
+  projectsList.push(newProject);
+  updateActiveProject(newProject);
+  UI.renderProjects(projectsList);
+  UI.renderTasks(activeProject);
+  Storage.save(projectsList);
 }
 
 function deleteProject(project) {
-    const projectIndex = projectsList.indexOf(project);
-    projectsList.splice(projectIndex, 1);
-    UI.renderProjects(projectsList);
-    updateActiveProject(projectsList[0]);
-    UI.renderTasks(activeProject);
-    Storage.save(projectsList);
+  const projectIndex = projectsList.indexOf(project);
+  projectsList.splice(projectIndex, 1);
+  UI.renderProjects(projectsList);
+  updateActiveProject(projectsList[0]);
+  UI.renderTasks(activeProject);
+  Storage.save(projectsList);
 }
 
 function addTask(title) {
-    const newTask = new Task(title);
-    newTask.dueDate = '';
-    activeProject.tasks.push(newTask);
-    UI.renderTasks(activeProject);
-    Storage.save(projectsList);
+  const newTask = new Task(title);
+  newTask.dueDate = "";
+  activeProject.tasks.push(newTask);
+  UI.renderTasks(activeProject);
+  Storage.save(projectsList);
 }
 
 function updateTask(task, title, completed, dueDate, priority) {
-    task.title = title;
-    task.completed = completed;
-    task.dueDate = dueDate;
-    task.priority = priority;
-    UI.renderTasks(activeProject);
-    Storage.save(projectsList);
+  const currentTask = task;
+  currentTask.title = title;
+  currentTask.completed = completed;
+  currentTask.dueDate = dueDate;
+  currentTask.priority = priority;
+  UI.renderTasks(activeProject);
+  Storage.save(projectsList);
 }
 
 function deleteTask(task) {
-    const taskIndex = activeProject.tasks.indexOf(task);
-    activeProject.tasks.splice(taskIndex, 1);
-    UI.renderTasks(activeProject);
-    Storage.save(projectsList);
-};
+  const taskIndex = activeProject.tasks.indexOf(task);
+  activeProject.tasks.splice(taskIndex, 1);
+  UI.renderTasks(activeProject);
+  Storage.save(projectsList);
+}
+
+function createDefaultProject() {
+  addProject("Inbox");
+  addTask("Create a new project");
+  addTask("Create a new task");
+  addTask("Mark a task as completed");
+}
 
 function loadProjects() {
-    const storedProjectsList = Storage.load();
-    if (storedProjectsList.length === 0) {
-        console.log('No projects found in local storage. Creating default project.');
-        createDefaultProject();
-    } else {
-        projectsList = storedProjectsList;
+  const storedProjectsList = Storage.load();
+  if (storedProjectsList.length === 0) {
+    console.log("No projects found in local storage. Creating default project.");
+    createDefaultProject();
+  } else {
+    projectsList = storedProjectsList;
+  }
+  updateActiveProject(projectsList[0]);
+  UI.renderProjects(projectsList);
+}
+
+function setupEventListeners() {
+  // Project Input Event Listeners
+  const projectSubmitButton = document.querySelector("#saveProjectBtn");
+  projectSubmitButton.addEventListener("click", () => {
+    const projectInputField = document.querySelector("#newProjectInput");
+    const projectTitle = projectInputField.value.trim();
+    if (projectTitle) {
+      addProject(projectTitle);
     }
-    updateActiveProject(projectsList[0]);
-    UI.renderProjects(projectsList);
+  });
+
+  // Task Input Event Listeners
+  const taskInputField = document.querySelector("#newTaskInput");
+  taskInputField.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (taskInputField.value) {
+        addTask(taskInputField.value);
+      }
+    }
+  });
 }
 
 setupEventListeners();
 loadProjects();
 
-//localStorage.clear();
+// localStorage.clear();
 
-export { updateActiveProject, deleteProject, updateTask, deleteTask };
+export {
+  updateActiveProject, deleteProject, updateTask, deleteTask,
+};
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,29 +1,29 @@
 class Task {
-    constructor(title) {
-        this.title = title;
-        this.completed = false;
-        this.dueDate = null;
-        this.priority = 'none';
-    }
+  constructor(title) {
+    this.title = title;
+    this.completed = false;
+    this.dueDate = null;
+    this.priority = "none";
+  }
 
-    // Method to convert the task to a JSON-compatible object
-    toJSON() {
-        return {
-            title: this.title,
-            completed: this.completed,
-            dueDate: this.dueDate,
-            priority: this.priority,
-        };
-    }
+  // Method to convert the task to a JSON-compatible object
+  toJSON() {
+    return {
+      title: this.title,
+      completed: this.completed,
+      dueDate: this.dueDate,
+      priority: this.priority,
+    };
+  }
 
-    // Method to create a Task instance from a JSON-compatible object
-    static fromJSON(jsonObj) {
-        const task = new Task(jsonObj.title);
-        task.completed = jsonObj.completed;
-        task.dueDate = jsonObj.dueDate;
-        task.priority = jsonObj.priority;
-        return task;
-    }
+  // Method to create a Task instance from a JSON-compatible object
+  static fromJSON(jsonObj) {
+    const task = new Task(jsonObj.title);
+    task.completed = jsonObj.completed;
+    task.dueDate = jsonObj.dueDate;
+    task.priority = jsonObj.priority;
+    return task;
+  }
 }
 
 export default Task;
